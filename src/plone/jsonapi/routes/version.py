aebd40168a866ef8aeeea7162bed5bce8ce5fcfe
Ramon Bartl <rb@ridingbytes.com>
2015-09-11 21:53:48 +0200

    bumped version

--- src/plone/jsonapi/routes/version.py
+++ src/plone/jsonapi/routes/version.py
@@ -13,8 +13,8 @@ def version():
     return dist.version
 
 __version__ = version()
-__build__ = 450
-__date__ = '2015-09-09'
+__build__ = 451
+__date__ = '2015-09-11'
 
 
 @add_plone_route("/version", "ploneapiversion", methods=["GET"])
