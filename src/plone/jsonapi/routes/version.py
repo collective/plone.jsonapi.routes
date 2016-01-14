519797ec8f16f9da1bef2d551215308389fafc26
José Dinuncio <jose.dinuncio@gmail.com>
2016-01-14 17:01:25 -0430

    Prepare release 0.8.4

--- src/plone/jsonapi/routes/version.py
+++ src/plone/jsonapi/routes/version.py
@@ -13,8 +13,8 @@ def version():
     return dist.version
 
 __version__ = version()
-__build__ = 465
-__date__ = '2015-12-08'
+__build__ = 466
+__date__ = '2016-01-14'
 
 
 @add_plone_route("/version", "ploneapiversion", methods=["GET"])
